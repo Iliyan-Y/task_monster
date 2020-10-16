58052af5b8cc16cad6227f81255fd03b938bff3f
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-10-16 12:52:36 +0100

    Created task list view state with mapped tasks including key to be used by swipe list view

--- frontend/components/Tasks/taskList.js
+++ frontend/components/Tasks/taskList.js
@@ -3,25 +3,41 @@ import { StyleSheet, View, TextInput, Text, Button } from 'react-native';
 import axios from 'axios';
 import { railsServer } from '../../serverAddress';
 import { TasksContext } from '../../context';
+// import { SwipeListView } from 'react-native-swipe-list-view';
 
 function TaskList({ navigation }) {
   let { taskList, setTaskList } = useContext(TasksContext);
   let [displayTask, setDisplayTask] = useState([]);
+  let [taskListView, setTaskListView] = useState([]);
+    
   function addTask() {
     navigation.navigate('Add Task');
   }
+    
   useEffect(() => {
-    setDisplayTask(taskList);
+      setDisplayTask(taskList)
+      setTaskListView(
+        taskList.map((task) => ({
+          key: task.title,
+          title: task.title,
+          description: task.description,
+          completed: task.completed,
+        }))
+      );
   }, [taskList]);
+    console.log(taskListView)
   return (
     <View>
-      <Text>Task list</Text>
-      {displayTask.map((task) => (
+          <Text>Task list</Text>
+          
+       {/* {displayTask.map((task) => (
         <View key={task.title}>
           <Text>{task.title}</Text>
           <Text>{task.description}</Text>
         </View>
-      ))}
+      ))} */}
+              
+             
       <Button onPress={() => addTask()} title="Add a new task" />
     </View>
   );
