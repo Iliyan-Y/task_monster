cf12700d46db20511b9190ff3dae19d66b10720e
Iliyan-Y <ili.yankov91@gmail.com>
2020-10-19 17:34:47 +0100

    Kind a working time

--- frontend/components/Tasks/taskHelpers.js
+++ frontend/components/Tasks/taskHelpers.js
@@ -32,19 +32,22 @@ export let calculateExpTime = (userInput) => {
   let userDate = userInput.split('T')[0];
   let userTime = userInput.split('T')[1].split('.')[0];
   let userHour = userTime.split(':')[0];
+  let userMins = parseInt(userTime.split(':')[1]);
+  let localMin = new Date().getMinutes();
+  let timeOffset = 0;
+
   let expiryTime;
 
   if (userInput.month == 0 && userInput.day == 0) {
     return 0;
   }
 
-  if (userHour == new Date().getHours()) {
+  if (userHour == new Date().getHours() && localMin - userMins > 3) {
     expiryTime = '23:59:59';
   } else {
     expiryTime = userTime;
+    timeOffset = 1;
   }
-
-  let mo = moment('18:49:59', 'HH:mm:ss').fromNow();
 
   let date = moment().format('YYYY-MM-DD HH:mm:ss');
 
@@ -52,11 +55,11 @@ export let calculateExpTime = (userInput) => {
   let expirydate = `${userDate} ${expiryTime}`;
   //difference of the expiry date-time given and current date-time
   let difference = moment.duration(moment(expirydate).diff(moment(date)));
-  console.log(difference);
+
   let hours = parseInt(difference.asHours());
   let minutes = parseInt(difference.minutes());
   let seconds = parseInt(difference.seconds());
-  finalTime = hours * 60 * 60 + minutes * 60 + seconds;
+  finalTime = (hours + timeOffset) * 60 * 60 + minutes * 60 + seconds;
 
   return finalTime;
 };
