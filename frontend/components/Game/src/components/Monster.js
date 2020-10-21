b6d0dee665c69b197dbdf3c9d38845a162baf981
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-10-21 14:45:03 +0100

    Added functions to change width and height based on sprite, make ghost bigger. Added breathing animation to monsters

--- frontend/components/Game/src/components/Monster.js
+++ frontend/components/Game/src/components/Monster.js
@@ -1,5 +1,5 @@
-import React, { useContext } from 'react';
-import { Image } from 'react-native';
+import React, { useState, useContext, useEffect } from 'react';
+import { Image, Animated, Easing } from 'react-native';
 import { array, object, string } from 'prop-types';
 import Matter from 'matter-js';
 import { TasksContext } from '../../../../context';
@@ -10,6 +10,7 @@ const monster3 = require('../../assets/monster3.png');
 const monster4 = require('../../assets/monster4.png');
 
 const Monster = (props) => {
+  const [scaleValue] = useState(new Animated.Value(1));
   let { score } = useContext(TasksContext);
   const width = props.size[0];
   const height = props.size[1];
@@ -30,20 +31,60 @@ const Monster = (props) => {
     }
   }
   function getWidth() {
-    
+    if (monsterSprite == monster0) {
+      return width + 50
+    } else {
+      return width + score * 10
+    }
   }
   function getHeight() {
-
+    if (monsterSprite == monster0) {
+      return height + 50;
+    } else {
+      return height + score * 10;
+    }
   }
+  function getTop() {
+    if (monsterSprite == monster0) {
+      return y - 30;
+    } else {
+      return y - score * 10;
+    }
+  }
+const monsterAnimation = () => {
+  Animated.sequence([
+    Animated.timing(scaleValue, {
+      toValue: 1.05,
+      duration: 500,
+      useNativeDriver: true,
+      easing: Easing.linear,
+    }),
+    Animated.timing(scaleValue, {
+      toValue: 1,
+      duration: 500,
+      useNativeDriver: true,
+      easing: Easing.linear,
+    }),
+  ]).start(() => monsterAnimation());
+};
+
+  React.useEffect(() => {
+    monsterAnimation();
+  }, []);
 
     return (
-      <Image
+      <Animated.Image
         style={{
           position: 'relative',
           left: x,
-          top: y - score * 10,
-          width: width + score * 10,
-          height: height + score * 10,
+          top: getTop(),
+          width: getWidth(),
+          height: getHeight(),
+          transform: [
+            {
+              scale: scaleValue,
+            },
+          ],
         }}
         resizeMode="stretch"
         source={monsterSprite}
