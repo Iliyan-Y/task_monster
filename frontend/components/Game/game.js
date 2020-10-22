4c034bfb928d087a04c35f1e856293b485f08e23
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-10-22 14:10:24 +0100

    Re-added game background

--- frontend/components/Game/game.js
+++ frontend/components/Game/game.js
@@ -4,6 +4,7 @@ import Entities from './src/entities';
 import { GameEngine } from 'react-native-game-engine';
 import Systems from './src/systems';
 import { Audio } from 'expo-av';
+const background = require('./assets/background.png');
 
 export default class Game extends PureComponent {
   constructor(props) {
@@ -39,6 +40,7 @@ export default class Game extends PureComponent {
   render() {
     return (
       <View style={styles.container}>
+        <Image source={background} />
         <GameEngine
           ref={(ref) => {
             this.gameEngine = ref;
